c5aa66f5943943304c5b93f4617f417da79be087
Judipie <hakesjude@gmail.com>
2026-05-14 10:18:12 -0400

    main json functionality finished

--- js/apps.js
+++ js/apps.js
@@ -26,48 +26,69 @@ xhrLocationsReq.onload = function() {
   if (xhrLocationsReq.status === 200) {
     const locResponseObject = JSON.parse(xhrLocationsReq.responseText);
 
-    let attCard = '';
-    let lanCard = '';
-    let grubCard = '';
+    let attCard = ''; let lanCard = ''; let grubCard = '';
+    let footAttCard = ''; let footLanCard = ''; let footGrubCard = '';
 
     for (let i = 0; i < locResponseObject.attractions.length; i++) {
       const Item = locResponseObject.attractions[i];
 
-      attCard +=
-        `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
-          <img src="${Item.image}" alt="${Item.alt}">
-          <p>${Item.description}</p>
-        </a>`;
+      attCard += `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
+      <img src="${Item.image}" alt="${Item.alt}"><p>${Item.description}</p></a>`;
+
+      footAttCard += `<a href="${Item.map}" target="_blank" rel="noopener noreferrer">${Item.name}</a>`;
     }
 
     for (let i = 0; i < locResponseObject.landmarks.length; i++) {
       const Item = locResponseObject.landmarks[i];
 
-      lanCard +=
-        `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
-          <img src="${Item.image}" alt="${Item.alt}">
-          <p>${Item.description}</p>
-        </a>`;
+      lanCard += `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
+      <img src="${Item.image}" alt="${Item.alt}"><p>${Item.description}</p></a>`;
+
+      footLanCard += `<a href="${Item.map}" target="_blank" rel="noopener noreferrer">${Item.name}</a>`;
     }
 
     for (let i = 0; i < locResponseObject.grub.length; i++) {
       const Item = locResponseObject.grub[i];
 
-      grubCard +=
-        `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
-          <img src="${Item.image}" alt="${Item.name}">
-          <p>${Item.description}</p>
-        </a>`;
+      grubCard += `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
+      <img src="${Item.image}" alt="${Item.name}"><p>${Item.description}</p></a>`;
+
+      footGrubCard += `<a href="${Item.map}" target="_blank" rel="noopener noreferrer">${Item.name}</a>`;
     }
 
     document.querySelector('#attractionContent').innerHTML = attCard;
     document.querySelector('#landmarkContent').innerHTML = lanCard;
     document.querySelector('#grubContent').innerHTML = grubCard;
+    document.querySelector('#attractionFooter').innerHTML = footAttCard;
+    document.querySelector('#landmarkFooter').innerHTML = footLanCard;
+    document.querySelector('#grubFooter').innerHTML = footGrubCard;
   }
 };
 
-xhrLocationsReq.open("GET", "/res/data/locations.json", true);
+xhrServicesReq.onload = function() {
+  if (xhrServicesReq.status === 200) {
+    const serResponseObject = JSON.parse(xhrServicesReq.responseText);
+
+    let serCard = ''; let footSerCard = '';
+
+    for (let i = 0; i < serResponseObject.services.length; i++) {
+      const Item = serResponseObject.services[i];
+
+      serCard += `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">
+      <img src="${Item.image}" alt="${Item.name} Logo" style="${Item.style}"></a>`;
+
+      footSerCard += `<a href="${Item.link}" target="_blank" rel="noopener noreferrer">${Item.name}</a>`;
+    }
+
+    document.querySelector('#servicesContent').innerHTML = serCard;
+    document.querySelector('#servicesFooter').innerHTML = footSerCard;
+  }
+};
+
+xhrLocationsReq.open("GET", "/../res/data/locations.json", true);
+xhrServicesReq.open("GET", "/../res/data/services.json", true);
 xhrLocationsReq.send();
+xhrServicesReq.send();
 
 const ohioArray = [];
 
